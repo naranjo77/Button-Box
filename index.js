6b349dcdf6dd37dcb2d28dc2600c2db8d8a72e70
naranjo77 <naranjo10677@gmail.com>
2023-12-25 00:13:06 -0800

    button box

---
+++ index.js
@@ -0,0 +1,8 @@
+document.querySelector(".clickme").addEventListener("click", () => {
+    document.querySelectorAll(".hidden").forEach((item) => {
+      item.classList.toggle("showing");
+    });
+});
+
+
+  
